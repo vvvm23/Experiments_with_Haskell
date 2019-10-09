e470cf97954128357fe4845b57249b3441dd1e43
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-10-09 11:37:37 +0100

    basic operators

---
+++ basic_operators.hs
@@ -0,0 +1,17 @@
+main = do
+    let var1 = 12
+    let var2 = 5
+    putStrLn "12 + 5 is: "
+    print (var1 + var2)
+
+    putStr "12 - 5 is: "
+    print (var1 - var2)
+
+    putStr "12 * 5 is: "
+    print (var1 * var2)
+
+    putStr "12 / 5 is: "
+    print (var1 / var2)
+
+    putStr "All numbers from 5 to 12 are: "
+    print [var2..var1]
