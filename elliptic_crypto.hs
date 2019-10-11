9bc39cd4a36f6c05631a50417e8a48ccf992dd7a
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-10-11 11:47:00 +0100

    moving elliptic crypto to a different repo

--- elliptic_crypto.hs
+++
@@ -1,5 +0,0 @@
-grad :: (Float, Float) -> (Float, Float) -> Float
-grad x1 y1 x2 y2 | y1 /= y2 = (x2 - x1) / (y2 - y1)
-                 | y1 == y2 = 1 / 0
-
-elliptic :: Float -> (Float, Float) -> Float
